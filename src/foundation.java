22994d75691eacae2a7798f6e45d7301160e305c
faithgarza <faith.garza001@gmail.com>
2020-02-10 15:09:06 -0600

    progress

--- src/foundation.java
+++ src/foundation.java
@@ -2,73 +2,26 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class foundation {
 
-    private static List<String> contact;
-    private String  firstName;
-    private String lastName;
-    private int contacts;
-    private String email;
-
-
-    public List<String> getContact() {
-        return contact;
-    }
-
-    public void setContact() {
-        this.contact = new ArrayList<>();
-    }
-
-
-    public String getFirst() {
-        return firstName;
-    }
-
-    public void setFirst(String firstName) {
-         this.firstName = firstName;
-    }
-
-    public String getLast() {
-        return lastName;
-    }
-
-   public void setLast(String lastName) {
-        this.lastName = lastName;
-   }
-
-   public int getContacts() {
-        return contacts;
-   }
-
-   public void setContacts(int contacts) {
-        this.contacts = contacts;
-   }
-
-   public String getEmail() {
-        return email;
-   }
-
-   public void setEmail(String email) {
-        this.email = email;
-   }
-
 
     public static void main(String[] args) {
-        ViewContacts viewAll = new ViewContacts();
-        newFile();
-        writeFile();
-        viewAll.mainMenu();
 
+        System.out.println(display());
     }
 
+    private static String display() {
+        return null;
+    }
+
+    public String display(ContactManager.User user) {
+        return "--------------------" +
+                "\n  name:  " + user.getContact() +
+                "\n  email: " + user.getEmail();
 
 
-
-        static void newFile(){
+        public void newFile() {
             String directory = "data";
             String filename = "contact.txt";
 
@@ -77,50 +30,52 @@ public class foundation {
             Path dataFile = Paths.get(directory, filename);
 
 
-
-            try{
-                if(Files.notExists(dataDirectory)) {
+            try {
+                if (Files.notExists(dataDirectory)) {
                     Files.createDirectories(dataDirectory);
                     System.out.println("Created Directory");
                 }
-                if(!Files.exists(dataFile)) {
+                if (!Files.exists(dataFile)) {
                     Files.createFile(dataFile);
                     System.out.println("Created file");
                 }
-            }catch (IOException ioe){
+            } catch (IOException ioe) {
                 ioe.printStackTrace();
             }
 
-        }
 
-
-    //program to interfaces versus implementation
-    public static void writeFile() {
-            contact.add("Jim");
-            contact.add("Joe");
-        for (String fr : contact)
-            System.out.println(fr);
-        try {
-            Path info = Paths.get("data", "contact.txt");
-            Files.write(info, contact);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-
-        contact.add("John"); contact.add("Jack");
-        try {
-            Path info = Paths.get("data", "contact.txt");
-            Files.write(info, contact);
-        } catch (IOException ioe){
-            ioe.printStackTrace();
+            //program to interfaces versus implementation
+//        public static void writeFile () {
+//            contact.add("Jim");
+//            contact.add("Joe");
+//            for (String fr : contact)
+//                System.out.println(fr);
+//            try {
+//                Path info = Paths.get("data", "contact.txt");
+//                Files.write(info, contact);
+//            } catch (IOException ioe) {
+//                ioe.printStackTrace();
+//            }
+//
+//            contact.add("John");
+//            contact.add("Jack");
+//            try {
+//                Path info = Paths.get("data", "contact.txt");
+//                Files.write(info, contact);
+//            } catch (IOException ioe) {
+//                ioe.printStackTrace();
+//            }
+//        }
         }
     }
 
 
+}
 
 
 
 
 
-    }
+
+
 
